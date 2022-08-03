18824db895360ba925868161d2dc422f021627a4
ranu-agita <ranuagitaf@gmail.com>
2022-08-03 07:01:31 +0700

    add db

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from "react-redux";
+
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
   
 );
---
+++ src/reducers/index.jsx
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+ import ProdukReducer from './product';
+
+ export default combineReducers({
+    ProdukReducer
+ })
